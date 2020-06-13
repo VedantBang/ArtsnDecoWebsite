de6e766d3e2c7aa07c5728b23e17116c42171e16
VedantBang <vedantbang5@gmail.com>
2020-06-13 15:36:53 +0530

    exit process on db connection error

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 async function connect() {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/data", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    console.log("Connected to \x1b[32mDatabase\x1b[0m");
-  } catch (err) {
-    console.log("\x1b[31mERROR\x1b[0m", err);
-  }
+	try {
+    	await mongoose.connect("mongodb://localhost:27017/data", {
+      		useUnifiedTopology: true,
+      		useNewUrlParser: true,
+    	});
+    	console.log("Connected to \x1b[32mDatabase\x1b[0m");
+  	} catch (err) {
+    	console.log("\x1b[31mERROR\x1b[0m", err);
+    	process.exit(0);
+  	}
 }
 module.exports = connect;
