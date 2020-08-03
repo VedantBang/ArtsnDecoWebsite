150529c43ade16422d219b96d54cdefa79ab3606
Rohit-Mundada <rohitrm0801@gmail.com>
2020-08-03 13:54:03 +0530

    Trying to implement Carousel, not working

--- public/js/displayFestByYear.js
+++ public/js/displayFestByYear.js
@@ -1,33 +1,27 @@
 (async () => {
   try {
     const response = await (
-      await fetch(`${url}/display/${localStorage.getItem("year")}`, {
-        method: "GET",
+      await fetch(`${url}/display/${localStorage.getItem('year')}`, {
+        method: 'GET',
       })
     ).json();
 
     console.log(response);
     for (let i = 0; i < response.data[0].images.length; i++) {
+      document.querySelector(
+        '.bg-image'
+      ).src = `${response.data[0].images[0].link}`;
       const carouselItem = `<div class="carousel-item active">
   <div class="view">
     <img
-      class="w-100 carousel-image"
-      src="${response.data[0].images[i].link}"
+      class="img-fluid"
+      src="${response.data[0].images[0].link}"
       alt="${response.data[0].images[i].title}"
     />
     <div class="mask rgba-black-light"></div>
   </div>
-  <div class="carousel-caption d-flex justify-content-end justify-content-center">
-    <h3 class="h3-responsive text-uppercase pr-5 mr-5">
-      <span class="text-uppercase">${response.data[0].fest}</span>
-      ${response.data[0].year}
-    </h3>
-    <h3 class="h3-responsive text-uppercase pl-5 ml-5">
-      ${response.data[0].images[i].title}
-    </h3>
-  </div>
 </div>`;
-      $(".fest-images").html(carouselItem);
+      $('.fest-images').html(carouselItem);
     }
   } catch (err) {
     console.log(err);
