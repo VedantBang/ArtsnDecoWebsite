643d7a15025a106233d947702054a4fd290c485d
Rohit-Mundada <rohitrm0801@gmail.com>
2020-08-04 22:00:17 +0530

    Carousel styling not working

--- public/js/displayFestByYear.js
+++ public/js/displayFestByYear.js
@@ -13,17 +13,21 @@
 
     const images = response.data[0].images;
 
+    document.querySelector('.bg-image').src = images[0].link;
+
     $(document).ready(function () {
       for (let i = 0; i < images.length; i++) {
         $(
           `<div class="carousel-item">
-            <img 
-              src=${images[i].link} 
-              class="img-fluid" 
-              alt=${images[i].title} 
-            />
-            <div class="carousel-caption">
-              <span class="card-text">${images[i].title}</span>
+            <div class="item__third">
+              <img 
+                src=${images[i].link} 
+                class="img-fluid" 
+                alt=${images[i].title} 
+              />
+              <div class="carousel-caption">
+                <span class="card-text">${images[i].title}</span>
+              </div>
             </div>
           </div>`
         ).appendTo('.carousel-inner');
@@ -40,3 +44,20 @@
     console.log(err);
   }
 })();
+
+// Implementing https://codepen.io/MhSami/pen/zNBMbj code
+$('.carousel-item', '.show-neighbors')
+  .each(function () {
+    let next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  })
+  .each(function () {
+    let prev = $(this).prev();
+    if (!prev.length) {
+      prev = $(this).sibilings(':last');
+    }
+    prev.children(':nth-last-child(2)').clone().prependTo($(this));
+  });
