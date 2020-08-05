aefd8137c5ccacae7ac40807aaea2f1a3250d8a6
Rohit-Mundada <rohitrm0801@gmail.com>
2020-08-05 16:53:30 +0530

    Cannot implement slick

--- public/js/util/coverWorks.js
+++ public/js/util/coverWorks.js
@@ -7,7 +7,7 @@ const coverWorks = (fest) => {
       ).json();
 
       for (let i = 0; i < response.data.length; i++) {
-        const cover = `<div class="col-12 col-sm-6 col-md-4 card w-40 mb-4">
+        const cover = `<div class="col-12 col-md-4 card mb-4">
             <a 
               href="posts.html"
               type="button" 
@@ -26,40 +26,14 @@ const coverWorks = (fest) => {
         $(`.${fest}-slider`).append(cover);
       }
 
-      $(`.${fest}-slider`).slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true,
-            },
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ],
-      });
+      // $(`.${fest}-slider`).slick({
+      //   infinite: true,
+      //   speed: 300,
+      //   slidesToShow: 3,
+      //   slidesToScroll: 3,
+      //   prevArrow: $('.prev'),
+      //   nextArrow: $('.next'),
+      // });
     } catch (err) {
       console.log(err);
     }
