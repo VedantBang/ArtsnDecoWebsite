57cdade440378a7473e3a6c5743097d430eba837
Rohit-Mundada <rohitrm0801@gmail.com>
2020-08-05 14:35:00 +0530

    Attempting multi carousel

--- public/js/util/coverWorks.js
+++ public/js/util/coverWorks.js
@@ -7,7 +7,7 @@ const coverWorks = (fest) => {
       ).json();
 
       for (let i = 0; i < response.data.length; i++) {
-        const cover = `<div class="col-12 col-sm-6 col-md-4 card w-40 mb-4">
+        $(`<div class="col-12 col-sm-6 col-md-4 card w-40 mb-4">
             <a 
               href="posts.html"
               type="button" 
@@ -22,9 +22,10 @@ const coverWorks = (fest) => {
               <div class="card-text year">${response.data[i].year}</div>
               <div class="card-text theme text-uppercase">${response.data[i].theme}</div>
             </a>
-          </div>`;
-        $(`#${fest}`).append(cover);
+          </div>`).appendTo(`#${fest}`);
+        // $(`#${fest}`).append(cover);
       }
+      $('.carousel-item').first().addClass('active');
     } catch (err) {
       console.log(err);
     }
