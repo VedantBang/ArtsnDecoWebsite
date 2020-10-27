43aff34e538e64feca4e455c8f79dba94c8eda0f
Rohit-Mundada <rohitrm0801@gmail.com>
2020-10-27 08:58:49 +0530

    Multiple carousel implemented!!

--- public/js/util/displayCreativeWorks.js
+++ public/js/util/displayCreativeWorks.js
@@ -1,14 +1,31 @@
 const displayCreativeWorks = async () => {
   try {
+    let swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
     const response = await (
       await fetch(`${url}/creative/all`, { method: 'GET' })
     ).json();
 
+    console.log(response);
+
     for (let i = 0; i < response.data.length; i++) {
-      const creativePost = `<div class="col-12 col-sm-6 col-md-4">
+      swiper.appendSlide(`<div class="swiper-slide">
             <img src="${response.data[i].link}" alt="${response.data[i].title}" class="card-img-top" />
-        </div>`;
-      $('.carousel').append(creativePost);
+        </div>`);
     }
   } catch (err) {
     console.log(err);
