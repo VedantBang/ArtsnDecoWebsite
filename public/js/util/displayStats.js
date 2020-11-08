7bfbdafc93e03f901c9aaaf0a85c4bad6b3f1f6c
Rohit-Mundada <rohitrm0801@gmail.com>
2020-11-08 15:11:34 +0530

    Stat typewriter effect added, styling improved

--- public/js/util/displayStats.js
+++ public/js/util/displayStats.js
@@ -4,33 +4,51 @@ const displayStats = async () => {
       await fetch(`${url}/stats/all`, { method: 'GET' })
     ).json();
 
-    // console.log(response);
-
-    // typeWriter(response.lines[0]);
-
     for (let i = 0; i < response.lines.length; i++) {
-      // if (response.lines[i]) {
-      //   typeWriter(`${response.lines[i]}`);
-      // }
-      const stat = `<h1 class="stat text-center">${response.lines[i]}</h1>`;
-      $('.stats').append(stat);
+      const stat = `<div class="stat text-center">${response.lines[i]}</div>`;
+      $(`.stats`).append(stat);
     }
+
+    // console.log(response.lines);
+
+    // for (let i = 0; i < response.lines.length; i++) {
+    //   const stat = document.createElement('div');
+    //   stat.classList.add('stat', 'text-center');
+    //   stat.id = `stat${i}`;
+    //   $('.stats').append(stat);
+    // }
+
+    // let stringCount = 0,
+    //   speed = 50;
+
+    // function statClosure(text) {
+    //   let txt = text;
+
+    //   console.log(txt);
+
+    //   let i = 0;
+
+    //   function typeWriter() {
+    //     let stringCountContext = stringCount;
+    //     if (i < txt.length) {
+    //       if (document.getElementById(`stat${stringCountContext}`)) {
+    //         document.getElementById(
+    //           `stat${stringCountContext}`
+    //         ).innerText += txt.charAt(i);
+    //         i++;
+    //         setTimeout(typeWriter, speed);
+    //       }
+    //     }
+    //   }
+
+    //   stringCount++;
+    //   return typeWriter;
+    // }
+
+    // for (let i = 0; i < response.lines.length; i++) {
+    //   statClosure(response.lines[i])();
+    // }
   } catch (err) {
     console.log(err);
   }
 };
-
-// let i = 0,
-//   speed = 50;
-// let txt = 'Is this working?';
-// const typeWriter = () => {
-//   if (i < txt.length) {
-//     document.querySelector('.stats').innerHTML += txt.charAt(i);
-//     i++;
-
-//     setTimeout(typeWriter, speed);
-//   }
-//   // $('.stats').append(stat);
-// };
-
-// typeWriter();
