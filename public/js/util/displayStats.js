24e8461a503c362ae4d142698cc1cfd80af4c937
Rohit-Mundada <rohitrm0801@gmail.com>
2020-11-26 19:16:10 +0530

    Added new animation to Stats

--- public/js/util/displayStats.js
+++ public/js/util/displayStats.js
@@ -1,58 +1,48 @@
 const displayStats = async () => {
-    try {
-        const response = await (
-            await fetch(`${url}/stats/all`, { method: 'GET' })
-        ).json();
+  try {
+    const response = await (
+      await fetch(`${url}/stats/all`, { method: 'GET' })
+    ).json();
 
-        for (let i = 0; i < response.data.length; i++) {
-            const stat = `<div class="col-md-3 col-sm-6 col-6 single-stat">
-        <img src="${response.data[i].icon}" alt="stat icon" class="stats-icon">
-        <h3 class="white-text"><span class="counter">${response.data[i].number}</span></h3>
-        <span class="white-text">${response.data[i].text}</span>
-    </div>`;
+    let regex = /^\d+(\.\d{1,2})?$/;
 
-            $(`.stats-main-content`).append(stat);
-        }
-    } catch (err) {
-        // console.log(response.lines);
+    for (let i = 0; i < response.data.length; i++) {
+      const text = response.data[i].number
+        .split('')
+        .filter((item) => !item.match(regex))
+        .join('');
+      const number = response.data[i].number
+        .split('')
+        .filter((item) => item.match(regex))
+        .join('');
+      const stat = `<div class="col-md-3 col-sm-6 col-6 single-stat">
+  <img src="${response.data[i].icon}" alt="stat icon" class="stats-icon" />
+  <h3 class="white-text">
+    <span id="stat-${i}"></span>
+    <span class="counter-text">${text}</span>
+  </h3>
+  <span class="white-text">${response.data[i].text}</span>
+</div>`;
 
-        // for (let i = 0; i < response.lines.length; i++) {
-        //   const stat = document.createElement('div');
-        //   stat.classList.add('stat', 'text-center');
-        //   stat.id = `stat${i}`;
-        //   $('.stats').append(stat);
-        // }
+      $(`.stats-main-content`).append(stat);
 
-        // let stringCount = 0,
-        //   speed = 50;
-
-        // function statClosure(text) {
-        //   let txt = text;
-
-        //   console.log(txt);
-
-        //   let i = 0;
-
-        //   function typeWriter() {
-        //     let stringCountContext = stringCount;
-        //     if (i < txt.length) {
-        //       if (document.getElementById(`stat${stringCountContext}`)) {
-        //         document.getElementById(
-        //           `stat${stringCountContext}`
-        //         ).innerText += txt.charAt(i);
-        //         i++;
-        //         setTimeout(typeWriter, speed);
-        //       }
-        //     }
-        //   }
-
-        //   stringCount++;
-        //   return typeWriter;
-        // }
-
-        // for (let i = 0; i < response.lines.length; i++) {
-        //   statClosure(response.lines[i])();
-        // }
-        console.log(err);
+      animateValue(document.getElementById(`stat-${i}`), 0, number, 2000);
     }
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.classList.add('counter');
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
