f6fd00af686949682498b555805db1ae8bf00a64
Rohit-Mundada <rohitrm0801@gmail.com>
2021-02-07 22:36:32 +0530

    Video paused and played after DOM loading, added video preload for smooth experience

--- public/js/util/loader.js
+++ public/js/util/loader.js
@@ -1,8 +1,10 @@
 document.onreadystatechange = function () {
   if (document.readyState !== 'complete') {
+    document.getElementById('myVideo').pause();
     document.querySelector('body').style.visibility = 'hidden';
     document.querySelector('.loader').style.visibility = 'visible';
   } else {
+    document.getElementById('myVideo').play();
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('body').style.visibility = 'visible';
   }
